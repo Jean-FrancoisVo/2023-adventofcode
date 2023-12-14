155a1cd9c08d5a85aeecebab1e62385e51ec7eb6
jeanfrancoisvo <jeanfrancoisvo7@gmail.com>
2023-12-14 20:28:05 +0100

    🔥 Day seven

--- src/main/kotlin/Day07.kt
+++ src/main/kotlin/Day07.kt
@@ -1,17 +1,44 @@
-data class HandBid(val value: String, val bid: Int) : Comparable<HandBid> {
+data class Rank(val card: Char) : Comparable<Rank> {
+    override fun compareTo(other: Rank): Int = position().compareTo(other.position())
+
+    private fun position(): Int = when (card) {
+        'A' -> 13
+        'K' -> 12
+        'Q' -> 11
+        'J' -> 10
+        'T' -> 9
+        '9' -> 8
+        '8' -> 7
+        '7' -> 6
+        '6' -> 5
+        '5' -> 4
+        '4' -> 3
+        '3' -> 2
+        '2' -> 1
+        else -> 0
+    }
+}
+
+data class HandBid(val hand: String, val bid: Int) : Comparable<HandBid> {
     override fun compareTo(other: HandBid): Int {
-        TODO("Not yet implemented")
+        return if (this.strength() == other.strength()) {
+            val s = hand.toCharArray().map { Rank(it) }
+            val o = other.hand.toCharArray().map { Rank(it) }
+            s.zip(o).map { it.first.compareTo(it.second) }.firstOrNull { it != 0 } ?: 0
+        } else {
+            this.strength().compareTo(other.strength())
+        }
     }
 
-    fun strength(): Int {
-        val hand = this.value.groupBy { it }.values.map { it.count() }
+    private fun strength(): Int {
+        val sameCardsCounts = this.hand.groupBy { it }.values.map { it.count() }
         return when {
-            hand.contains(5) -> 7
-            hand.contains(4) -> 6
-            hand.contains(3) && hand.contains(2) -> 5
-            hand.contains(3) -> 4
-            hand.count { it == 2 } == 2 -> 3
-            hand.contains(2) -> 2
+            sameCardsCounts.contains(5) -> 7
+            sameCardsCounts.contains(4) -> 6
+            sameCardsCounts.contains(3) && sameCardsCounts.contains(2) -> 5
+            sameCardsCounts.contains(3) -> 4
+            sameCardsCounts.count { it == 2 } == 2 -> 3
+            sameCardsCounts.contains(2) -> 2
             else -> 1
         }
     }
@@ -20,12 +47,8 @@ data class HandBid(val value: String, val bid: Int) : Comparable<HandBid> {
 fun main() {
     fun part1(input: List<String>): Int {
         val handsAndBids = input.map { it.split(" ") }.map { HandBid(it.first(), it.last().toInt()) }
-        println(handsAndBids[4])
-        println(handsAndBids[4].strength())
-//        val handStrengths = handsAndBids.map { p -> p.value.groupBy { it }}
-//        println(handStrengths[2].values.map { it.size })
-//        println(handStrengths)
-        return 1
+        println(handsAndBids.sorted().mapIndexed { index, handBid -> (index + 1) * handBid.bid })
+        return handsAndBids.sorted().mapIndexed {index, handBid -> (index + 1) * handBid.bid }.sum()
     }
 
     fun part2(input: List<String>): Int {
